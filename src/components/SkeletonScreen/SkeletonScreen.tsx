b5f79c028599e46ef63531cbef4ae0feeaca0313
srsexton94 <srsexton94@gmail.com>
2025-08-31 19:56:30 -0400

    add form to skeleton screen

--- src/components/SkeletonScreen/SkeletonScreen.tsx
+++ src/components/SkeletonScreen/SkeletonScreen.tsx
@@ -1,15 +1,22 @@
 import "./SkeletonScreen.css";
 
 function SkeletonScreen() {
+  const formEls: number[] = Array.from({ length: 5 }, (_v, i) => i)
   const cards: number[] = Array.from({ length: 20 }, (_v, i) => i);
+
   return (
-    <ul className="skeleton-list" aria-busy="true" aria-live="polite">
+    <div aria-busy="true" aria-live="polite">
+    <div className="skeleton-form">
+      {formEls.map(el => <div className="skeleton-item form-el" key={`skeleton-form-el-${el}`}></div>)}
+    </div>
+    <ul className="skeleton-list">
       {cards.map((card) => (
         <li key={`skeleton-card-${card}`}>
-          <div className="skeleton-card"></div>
+          <div className="skeleton-item card"></div>
         </li>
       ))}
     </ul>
+    </div>
   );
 }
 export default SkeletonScreen;
