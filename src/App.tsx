7ef28b41fd9d63d570a076e96561be9a6a9cc38b
srsexton94 <srsexton94@gmail.com>
2025-08-30 21:30:18 -0400

    add error and loading handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,38 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import api from "./api";
 import { seedLocalDatabase } from "./api/data/seed";
+import { type Patient } from "./api/data/patients";
+
+seedLocalDatabase();
 
 function App() {
-  seedLocalDatabase();
-  const [data, setData] = useState(null);
+  const [patients, setPatients] = useState<Patient[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await api.get("/api/patients");
-        setData(response.data);
+        setPatients(response.data);
         console.log("API Response:", response.data);
       } catch (err) {
+        if (err instanceof Error) setError(err);
         console.error("Error fetching data:", err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
+  if (error) return <p>An error occurred</p>;
+
   return (
     <>
       <h1>KinderClinic</h1>
-      <pre>{JSON.stringify(data)}</pre>
+      {isLoading ? <p>Loading...</p> : <pre>{JSON.stringify(patients)}</pre>}
     </>
   );
 }
